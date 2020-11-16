09e241632bd9b11c8b33b92818866d5cda4040ee
YAMAMI Taro <mitarothanken@gmail.com>
2020-11-16 15:52:00 +0900

    Starting with Spring Initializr - Securing
    https://spring.io/guides/gs/securing-web/

---
+++ demo/src/main/java/com/example/demo/securingweb/MvcConfig.java
@@ -0,0 +1,9 @@
+package com.example.demo.securingweb;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MvcConfig {
+    
+}

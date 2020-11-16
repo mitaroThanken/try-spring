7eeedac9921b51850358e5e2ae0e5d778fce6cf1
YAMAMI Taro <mitarothanken@gmail.com>
2020-11-16 17:43:38 +0900

    ゴミが残ってた

--- demo/src/main/java/com/example/demo/securingweb/MvcConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.securingweb;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-public class MvcConfig {
-    
-}
